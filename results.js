aad357420aac7400be01b23c1d52ad65d602ce38
heitz renaud <renaudheitz@hotmail.fr>
2018-06-06 15:00:12 +0700

    display the average propagation time, and avg propagation time regarding the size of paquets

--- results.js
+++ results.js
@@ -31,7 +31,7 @@ async function run(){
 async function writeFile(data, filename) {
     try{
         await writeFilePromise(filename, data);
-        console.log("The file was saved!");
+        console.log("Remote result files are saved!");
     }catch(err){
         console.log(err);
     }
@@ -42,7 +42,6 @@ async function downloadFile(address, filename) {
     // Get the data from url
     var data = "";
     var url = "http://"+address+":8100/"+filename;
-    console.log(url)
 
     var response = await axios.get(url);
     var data = response.data;
@@ -55,6 +54,7 @@ async function downloadFile(address, filename) {
 
 async function getAllFiles(address) {
     var index = 0;
+    console.log("Start downloading the remote result files");
     while(address[index]!=null){
         var destination = address[index].split(",");
         await downloadFile(destination[0],'result.csv' );
@@ -63,6 +63,7 @@ async function getAllFiles(address) {
 }
 
 async function calculation(address){
+    console.log("Start calculations");
     var index = 0;
     //Get logs from loadtest.js
     var loadTestData = await readFilePromise("results/transactionList.csv",'utf8')
@@ -77,22 +78,54 @@ async function calculation(address){
         var fileData = await readFilePromise(filePath,'utf8');
         var fileParsed = Papa.parse(fileData).data;
         // compare the two files
+        var sumPropagationTime = 0;
         
-        var sumPropagationTime= 0;
-        
-        for(var i in loadTestParsed,fileParsed)
+        var sumPropagationTime128 = 0;
+        var sumPropagationTime512 = 0;
+        var sumPropagationTime1024 = 0;
+
+        var iterration128 = 0;
+        var iterration512 = 0;
+        var iterration1024 = 0;
+        var iterration = 0;
+
+        for(var i in loadTestParsed)
         {
-           if(loadTestParsed[i] != null && fileParsed[i] != null && loadTestParsed[i][1] == fileParsed[i][1])
+            for(var j in fileParsed)
             {
-                var propagationTime = fileParsed[i][0] - loadTestParsed[i][0];
-                sumPropagationTime += propagationTime;
+                if(loadTestParsed[i] && fileParsed[j] && loadTestParsed[i][1] == fileParsed[j][1])
+                {
+                    
+                    if(loadTestParsed[i][4]==128){
+                        var propagationTime128 = fileParsed[j][0] - loadTestParsed[i][0];
+                        sumPropagationTime128 += propagationTime128;
+                        iterration128 ++;
+                    }
+                    else if(loadTestParsed[i][4]==512){
+                        var propagationTime512 = fileParsed[j][0] - loadTestParsed[i][0];
+                        sumPropagationTime512 += propagationTime512;
+                        iterration512 ++;
+                    }
+                    if(loadTestParsed[i][4]==1024){
+                        var propagationTime1024 = fileParsed[j][0] - loadTestParsed[i][0];
+                        sumPropagationTime1024 += propagationTime1024;
+                        iterration1024 ++;
+                    }
+                    
+                    var propagationTime = fileParsed[j][0] - loadTestParsed[i][0];
+                    sumPropagationTime += propagationTime ;
+                    iterration ++;
+                    
+                }
             }
-            
         }
         
-        var avgPropagationTime = sumPropagationTime / i;
-        console.log("Average propagation time of node : " +destinationPair[0] + " = " + avgPropagationTime )
+        var avgPropagationTime = sumPropagationTime / iterration;
         
+        console.log("Average propagation time of node  " +destinationPair[0] + " = " + avgPropagationTime )
+        console.log("Average propagation time of 128bits paquets of node  " +destinationPair[0] + " = " + (sumPropagationTime128/iterration128) )
+        console.log("Average propagation time of 512bits paquets of node  " +destinationPair[0] + " = " + (sumPropagationTime512/iterration512) )
+        console.log("Average propagation time of 1024bits paquets of node  " +destinationPair[0] + " = " +(sumPropagationTime1024/iterration1024) )
         
         index +=1 ;
     }
@@ -100,7 +133,7 @@ async function calculation(address){
 
 async function main(){
      run();
-
+ 
 }
 
 main();
